83dffef76dccbe6d721fa1be7e6a2b1f1c2fb02d
EpocDotFr <contact@epoc.fr>
2024-05-29 09:37:40 +0200

    Squash

--- README.md
+++ README.md
@@ -1,32 +1,7 @@
-[![progress-banner](https://backend.codecrafters.io/progress/shell/2aaae510-c25f-40ef-9e7c-6e01ba138103)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
+[![progress-banner](https://backend.codecrafters.io/progress/shell/2aaae510-c25f-40ef-9e7c-6e01ba138103)](https://app.codecrafters.io/users/EpocDotFr)
 
-This is a starting point for Python solutions to the
-["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview).
+Source code of my solution to the CodeCrafters' "[Build your own Shell](https://codecrafters.io/challenges/shell)"
+challenge.
 
-_Add a description of your course here_
-
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
-
-# Passing the first stage
-
-The entry point for your `shell` implementation is in `app/main.py`. Study and
-uncomment the relevant code, and push your changes to pass the first stage:
-
-```sh
-git add .
-git commit -m "pass 1st stage" # any msg
-git push origin master
-```
-
-Time to move on to the next stage!
-
-# Stage 2 & beyond
-
-Note: This section is for stages 2 and beyond.
-
-1. Ensure you have `python (3.11)` installed locally
-1. Run `./your_shell.sh` to run your program, which is implemented in
-   `app/main.py`.
-1. Commit your changes and run `git push origin master` to submit your solution
-   to CodeCrafters. Test output will be streamed to your terminal.
+I've put myself a personal challenge: do not use any third-party packages (only use built-in Python features). Of course,
+I did not use Python's `Cmd` or `shlex` neither.
--- app/main.py
+++ app/main.py
@@ -1,17 +1,70 @@
-import sys
+from os.path import basename
+from subprocess import call
+from os import getenv
+from glob import glob
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def main() -> None:
+    executables = {
+        directory: [
+            basename(executable) for executable in glob(directory + '/*')
+        ] for directory in getenv('PATH').split(':')
+    }
 
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    while True:
+        try:
+            line = input('$ ').split(' ')
 
-    # Wait for user input
-    input()
+            if not line:
+                print('Could not parse input')
+            elif line[0] == 'exit':
+                exit(
+                    int(line[1]) if len(line) == 2 else 0
+                )
+            elif line[0] == 'echo':
+                print(' '.join(line[1:]))
+            elif line[0] == 'type':
+                target = line[1] if len(line) == 2 else None
+
+                if target in ('exit', 'echo', 'type'):
+                    print(f'{target} is a shell builtin')
+                else:
+                    found = False
+
+                    for directory, execs in executables.items():
+                        if target in execs:
+                            print(f'{target} is {directory}/{target}')
+
+                            found = True
+
+                            break
+
+                    if not found:
+                        print(f'{target} not found')
+            else:
+                executable = None
+
+                for directory, execs in executables.items():
+                    if line[0] in execs:
+                        executable = f'{directory}/{line[0]}'
+
+                        break
+
+                if executable:
+                    call(
+                        '{} {}'.format(
+                            executable,
+                            ' '.join(line[1:] if len(line) > 1 else [])
+                        ),
+                        shell=True
+                    )
+                else:
+                    print(f'{line[0]}: command not found')
+        except (KeyboardInterrupt, EOFError):
+            exit(130)
+
+    exit(0)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
